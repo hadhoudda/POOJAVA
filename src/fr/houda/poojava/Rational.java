c252876c23c669e565f28c0965af28daacc56609
Houda Ben Abdallah <ahhouda@yahoo.fr>
2023-12-28 19:53:38 +0100

    commit 2

--- src/fr/houda/poojava/Rational.java
+++ src/fr/houda/poojava/Rational.java
@@ -1,8 +1,53 @@
 package fr.houda.poojava;
 
-public class Rational {
 
-	int numerator;
-	int denominator;
+public class Rational {
+		// les attribus de class Rational
+	private int numerator;
+	private int denominator;
+	
+	public int getNumerator() {
+		return numerator;
+	}
+	public void setNumerator(int numerator) {
+		this.numerator = numerator;
+	}
+	
+	public int getDenominator() {
+		return denominator;
+	}
+	public void setDenominator(int denominator) {
+		if(denominator == 0) {
+			throw new RuntimeException("denominator cannot be null");
+		}
+		this.denominator = denominator;
+	}
+	
+	
+	
+//	// 
+// pour créér class
+//	public Rational() {
+//		this(0, 1);
+////		this.setNumerator(0);
+////		this.setDenominator(1);
+//	}
+//	public Rational(int numerator, int denominator) {
+//		//super();
+//		this.setNumerator(numerator);
+//		this.setDenominator(denominator);
+//	}
+
+//	public int getDenominator() {
+//		return denominator;
+//	}
+
+//	@Override
+//	public String toString() {
+//		return String.format("%d/%d", this.numerator, this.denominator);
+//	}
+//	
+//	
+
 
 }
--- src/fr/houda/poojava/Start.java
+++ src/fr/houda/poojava/Start.java
@@ -3,11 +3,20 @@ package fr.houda.poojava;
 public class Start {
 
 	public static void main(String[] args) {
-		Rational r = new Rational();
-		r.numerator = 3;
-		r.denominator = 8;
-		System.out.printf("%d/%d\n", r.numerator, r.denominator);
-
-	}
+//		Rational r = new Rational();
+//		r.setNumerator(3);
+//		r.setDenominator(8);
+//		Rational r1 = new Rational(1,8);
+//		//Rational r2 = new Rational(5,0);
+//		Rational r3 = new Rational();
+//		//System.out.printf("%d/%d\n", r1.getNumerator(), r1.getDenominator());
+//		//System.out.printf("%d/%d\n", r.getNumerator(), r.getDenominator());
+//		//System.out.printf("%d/%d\n", r2.getNumerator(), r2.getDenominator());
+//		//System.out.printf("%d/%d\n", r3.getNumerator(), r3.getDenominator());
+//		System.out.println(r.toString());
+//		System.out.println(r1.toString());
+//		//System.out.println(r2.toString());
+//		System.out.println(r3.toString());
+		}
 
 }

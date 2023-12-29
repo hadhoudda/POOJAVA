2004afb3656f2dcf3f1aee5cec36a3c8de7cdfb4
Houda Ben Abdallah <ahhouda@yahoo.fr>
2023-12-29 11:53:51 +0100

    commit 3

--- src/fr/houda/poojava/Rational.java
+++ src/fr/houda/poojava/Rational.java
@@ -6,15 +6,32 @@ public class Rational {
 	private int numerator;
 	private int denominator;
 	
+	//------------ Costructeur de classe -------------------//
+	public Rational() {
+		this(0, 1); //valeur de constructeur par défaut
+		//this.setNumerator(0);
+		//this.setDenominator(1);
+	}
+		
+	public Rational(int numerator, int denominator) {
+		super();
+		this.setNumerator(numerator);
+		this.setDenominator(denominator);
+	}
+
+	//------------ get et set des attributs du classe -------------------//
+	
 	public int getNumerator() {
-		return numerator;
+		return this.numerator;
+		//return numerator; normalement comme ça main on ajoute this c'est mieux
 	}
 	public void setNumerator(int numerator) {
 		this.numerator = numerator;
 	}
 	
 	public int getDenominator() {
-		return denominator;
+		return this.denominator;
+		//return denominator; normalement comme ça main on ajoute this c'est mieux
 	}
 	public void setDenominator(int denominator) {
 		if(denominator == 0) {
@@ -23,31 +40,23 @@ public class Rational {
 		this.denominator = denominator;
 	}
 	
+	//------------ Méthode de classe -------------------//
+
+	@Override
+	public String toString() {
 	
+		return String.format("[%d/%d]", this.numerator, this.denominator);
+		// on ajoute this c'est mieux
+	}
 	
-//	// 
-// pour créér class
-//	public Rational() {
-//		this(0, 1);
-////		this.setNumerator(0);
-////		this.setDenominator(1);
-//	}
-//	public Rational(int numerator, int denominator) {
-//		//super();
-//		this.setNumerator(numerator);
-//		this.setDenominator(denominator);
-//	}
-
-//	public int getDenominator() {
-//		return denominator;
-//	}
-
-//	@Override
-//	public String toString() {
-//		return String.format("%d/%d", this.numerator, this.denominator);
-//	}
-//	
-//	
-
-
+	public Rational add(Rational r2) {
+		return new Rational(
+				(this.numerator*r2.denominator+r2.numerator*this.denominator),
+				(this.denominator*r2.denominator));	
+	}
+	
+	public boolean maxi(Rational r2) {
+		return(this.numerator*r2.denominator > r2.numerator*this.denominator);
+	}
+	
 }
--- src/fr/houda/poojava/Start.java
+++ src/fr/houda/poojava/Start.java
@@ -3,20 +3,34 @@ package fr.houda.poojava;
 public class Start {
 
 	public static void main(String[] args) {
-//		Rational r = new Rational();
-//		r.setNumerator(3);
-//		r.setDenominator(8);
-//		Rational r1 = new Rational(1,8);
-//		//Rational r2 = new Rational(5,0);
-//		Rational r3 = new Rational();
-//		//System.out.printf("%d/%d\n", r1.getNumerator(), r1.getDenominator());
-//		//System.out.printf("%d/%d\n", r.getNumerator(), r.getDenominator());
-//		//System.out.printf("%d/%d\n", r2.getNumerator(), r2.getDenominator());
-//		//System.out.printf("%d/%d\n", r3.getNumerator(), r3.getDenominator());
-//		System.out.println(r.toString());
-//		System.out.println(r1.toString());
-//		//System.out.println(r2.toString());
-//		System.out.println(r3.toString());
+		Rational r = new Rational();
+		r.setNumerator(3);
+		r.setDenominator(8);
+		Rational r1 = new Rational(2,7);
+		//Rational r2 = new Rational(5,0);
+		Rational r3 = new Rational();
+		//System.out.printf("%d/%d\n", r.getNumerator(), r.getDenominator());
+		//System.out.printf("%d/%d\n", r1.getNumerator(), r1.getDenominator());
+		//System.out.printf("%d/%d\n", r2.getNumerator(), r2.getDenominator());
+		//System.out.printf("%d/%d\n", r3.getNumerator(), r3.getDenominator());
+		System.out.println(r.toString());
+		System.out.println(r1.toString());
+		//System.out.println(r2.toString());
+		System.out.println(r3.toString());
+		
+		//----------- appel de méthode -----------//
+		System.out.println("---------------------------------------");
+		Rational r4 = new Rational(3, 6);
+		System.out.println(r4.toString());
+		Rational result = r4.add(r1);
+		System.out.println(r4 + "+" + r1 + "=" + result);
+		//System.out.println(r4.add(r1).toString());
+		
+		boolean res = r4.maxi(r1);
+		System.out.println(r4 + ">" + r1 + ":" + res);
+		
+		
 		}
+	
 
 }
